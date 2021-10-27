1931d423b9a4bb19178fa44dcb4a3d271bb4c4da
Denis A. Engemann <denis-alexander.engemann@inria.fr>
2021-10-27 07:45:03 +0200

    fix plotting script

--- plot_benchmark_age_prediction.r
+++ plot_benchmark_age_prediction.r
@@ -10,40 +10,40 @@ results$X <- NULL
 
 # order dataset/benchmark by average scores
 data_agg <- aggregate(
-  score ~ dataset,
-  subset(results, metric == 'r2' & benchmark != 'dummy'),
+  r2 ~ dataset,
+  subset(results, benchmark != 'dummy'),
   FUN = mean)
 
-data_order <- data_agg$dataset[order(data_agg$score)]
+data_order <- data_agg$dataset[order(data_agg$r2)]
 results$dataset <- factor(results$dataset, levels = data_order)
 
 bench_agg <- aggregate(
-  score ~ benchmark,
-  subset(results, metric == 'r2'),
+  r2 ~ benchmark,
+  results,
   FUN = mean)
 
-bench_order <- bench_agg$benchmark[order(bench_agg$score)]
+bench_order <- bench_agg$benchmark[order(bench_agg$r2)]
 results$benchmark <- factor(results$benchmark, levels = rev(bench_order))
 
 ggplot(
-  aes(x = score, y = benchmark, color = benchmark,
+  aes(x = r2, y = benchmark, color = benchmark,
       fill = benchmark,
       shape = dataset) ,
-  data = subset(results, metric == 'r2')) +
+  data = results) +
   facet_wrap(dataset~., nrow = 4, strip.position="right") +
   geom_jitter(show.legend = F) +
   geom_boxplot(alpha=0.1, show.legend = F) +
   theme_minimal(base_size = 18) +
+  coord_cartesian(xlim=c(-0.5, 1)) +
   scale_color_colorblind() +
   scale_fill_colorblind() +
   labs(y = element_blank(), x = "age prediction R2 [10-fold CV]")
 
-
 ggplot(
-  aes(x = score, y = benchmark, color = benchmark,
+  aes(x = MAE, y = benchmark, color = benchmark,
       fill = benchmark,
       shape = dataset) ,
-  data = subset(results, metric == 'r2')) +
+  data = results) +
   facet_wrap(dataset~., nrow = 4, strip.position="right") +
   geom_jitter(show.legend = F) +
   geom_boxplot(alpha=0.1, show.legend = F) +

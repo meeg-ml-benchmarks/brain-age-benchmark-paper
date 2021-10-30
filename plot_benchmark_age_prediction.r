10c204a3195cd4dfd7386ca532d951d59fbbd4da
Denis A. Engemann <denis-alexander.engemann@inria.fr>
2021-10-30 08:21:44 +0200

    update plotting

--- plot_benchmark_age_prediction.r
+++ plot_benchmark_age_prediction.r
@@ -11,30 +11,19 @@ results_fnames <- list.files('results/', full.names = T)
 results <- do.call(rbind, lapply(results_fnames, read.csv))
 results$X <- NULL
 
-# order dataset/benchmark by average scores
-data_agg <- aggregate(
-  r2 ~ dataset,
-  subset(results, benchmark != 'dummy'),
-  FUN = mean)
-
-data_order <- data_agg$dataset[order(data_agg$r2)]
-results$dataset <- factor(results$dataset, levels = data_order)
-
-bench_agg <- aggregate(
-  r2 ~ benchmark,
-  results,
-  FUN = mean)
-
-bench_order <- bench_agg$benchmark[order(bench_agg$r2)]
-results$benchmark <- factor(results$benchmark, levels = rev(bench_order))
-
 colors <- setNames(
   colorblind_pal()(8),
   c('black', 'orange', 'skye_blue', 'bluish_green', 'yellow', 'blue',
     'vermillon', 'purple'))
 
-color_values <- as.vector(colors[c('bluish_green', 'orange', 'skye_blue',
-                                   'black', 'vermillon', 'purple')])
+
+results$benchmark <- factor(
+  results$benchmark,
+  levels = c('deep', 'shallow', 'filterbank-source', 'filterbank-riemann', 'handcrafted','dummy'))
+
+
+color_values <- as.vector(colors[c('vermillon', 'orange', 'blue',
+                                   'skye_blue', 'bluish_green', 'black')])
 
 set.seed(42)
 (fig_r2 <- ggplot(
@@ -42,18 +31,18 @@ set.seed(42)
       fill = benchmark),
       data = results) +
   facet_wrap(dataset~., nrow = 4, strip.position="right") +
-  geom_jitter(show.legend = F, size = 1.7, alpha = 0.7) +
-  geom_boxplot(alpha=0.1, show.legend = F, size = 1.2, width = 0.9) +
-  theme_minimal(base_size = 24) +
-  coord_cartesian(xlim=c(-0.5, 1)) +
-  scale_y_discrete(labels = element_blank()) +
+  geom_jitter(show.legend = F, size = 1.2, alpha = 0.7) +
+  geom_boxplot(alpha=0.1, show.legend = F, size = 1.1, width = 0.8) +
+  theme_minimal(base_size = 16) +
+  coord_cartesian(xlim=c(-0.3, 0.8)) +
+  scale_x_continuous(breaks = seq(-0.5, 0.8, 0.1)) +
   scale_color_manual(values = color_values) +
   scale_fill_manual(values = color_values) +
   geom_vline(xintercept=0, color = 'black', linetype='dashed') +
-  labs(y = element_blank(), x = expression(R^2), title = 'B')) +
-  theme(plot.title = element_text(face = 'bold'))
+  labs(y = element_blank(), x = bquote(R^2~"[10-fold cross validation]"),
+       title = 'Age Prediction From M/EEG Signals'))
 
-# my_ggsave('./figures/fig_performance_r2', fig_r2, dpi = 300, width = 10, height = 8)
+my_ggsave('./figures/fig_performance_r2', fig_r2, dpi = 300, width = 8, height = 6)
 
 
 set.seed(42)
@@ -62,64 +51,13 @@ set.seed(42)
       fill = benchmark),
       data = results) +
   facet_wrap(dataset~., nrow = 4, strip.position="right") +
-  geom_jitter(show.legend = F, size = 1.7, alpha = 0.7) +
-  geom_boxplot(alpha=0.1, show.legend = F, size = 1.2, width = 0.9) +
-  theme_minimal(base_size = 24) +
+  geom_jitter(show.legend = F, size = 1.2, alpha = 0.7) +
+  geom_boxplot(alpha=0.1, show.legend = F, size = 1.1, width = 0.8) +
+  theme_minimal(base_size = 16) +
+  scale_x_continuous(breaks = seq(0, 25, 1)) +
   scale_color_manual(values = color_values) +
   scale_fill_manual(values = color_values) +
-  labs(y = element_blank(), x = "MAE", title = 'A')) +
-  theme(plot.title = element_text(face = 'bold'))
+  labs(y = element_blank(), x = "MAE [10-fold cross validation]",
+      title = 'Age Prediction From M/EEG Signals'))
 
-
-p <- fig_mae | fig_r2
-p + plot_annotation(
-  title = 'Age Prediction From M/EEG [10-fold cross validation]',
-  theme = theme(plot.title = element_text(size = 26))
-)
-
-my_ggsave('./figures/fig_performance', p, dpi = 300,
-          width = 12, height = 7.5)
-
-
-set.seed(42)
-(fig_fit <- ggplot(
-  aes(x = fit_time, y = benchmark, color = benchmark,
-      fill = benchmark),
-      data = results) +
-  facet_wrap(dataset~., nrow = 4, strip.position="right") +
-  geom_jitter(show.legend = F, size = 1.7, alpha = 0.7) +
-  geom_boxplot(alpha=0.1, show.legend = F, size = 1.2, width = 0.9) +
-  theme_minimal(base_size = 24) +
-  scale_x_log10() +
-  scale_color_manual(values = color_values) +
-  scale_fill_manual(values = color_values) +
-  labs(y = element_blank(), x = "fit time [seconds]",
-       title = 'A')) +
-  theme(plot.title = element_text(face = 'bold'))
-
-set.seed(42)
-(fig_score <- ggplot(
-  aes(x = score_time, y = benchmark, color = benchmark,
-      fill = benchmark),
-      data = results) +
-  facet_wrap(dataset~., nrow = 4, strip.position="right") +
-  geom_jitter(show.legend = F, size = 1.7, alpha = 0.7) +
-  geom_boxplot(alpha=0.1, show.legend = F, size = 1.2, width = 0.9) +
-  theme_minimal(base_size = 24) +
-  scale_x_log10() +
-  scale_y_discrete(labels = element_blank()) +
-  scale_color_manual(values = color_values) +
-  scale_fill_manual(values = color_values) +
-  labs(y = element_blank(), x = "score time [seconds]",
-       title = 'B')) +
-  theme(plot.title = element_text(face = 'bold'))
-
-p2 <- fig_fit | fig_score
-p2 + plot_annotation(
-  title = 'Run time [10-fold cross validation]',
-  theme = theme(plot.title = element_text(size = 26))
-)
-
-my_ggsave('./figures/fig_runtime', p2, dpi = 300,
-          width = 12, height = 7.5)
-# my_ggsave('./figures/fig_performance_mae', fig, dpi = 300, width = 10, height = 8)
+my_ggsave('./figures/fig_performance_mae', fig_mae, dpi = 300,  width = 8, height = 6)

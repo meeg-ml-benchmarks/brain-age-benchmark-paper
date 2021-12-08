cd6f453c1a2cc7c5770b60a81c1bc53655160a02
dengemann <denis.engemann@gmail.com>
2021-12-08 13:44:46 +0100

    tweak figures

--- plot_benchmark_age_prediction.r
+++ plot_benchmark_age_prediction.r
@@ -15,7 +15,7 @@ results$X <- NULL
 results$dataset <- factor(
   results$dataset,
   levels = c('camcan', 'lemon', 'chbp', 'tuab'),
-  labels = c('Cam-CAN', 'LEMON', 'CHBP', 'TUAB'))
+  labels = c('Cam-CAN\n(MEG)', 'LEMON\n(EEG)', 'CHBP\n(EEG)', 'TUAB\n(EEG)'))
 
 colors <- setNames(
   colorblind_pal()(8),
@@ -32,13 +32,13 @@ color_values <- as.vector(colors[c('vermillon', 'orange', 'blue',
                                    'skye_blue', 'bluish_green', 'black')])
 
 set.seed(42)
-(fig_r2 <- ggplot(
+fig_r2 <- ggplot(
   aes(x = r2, y = benchmark, color = benchmark,
       fill = benchmark),
       data = results) +
   facet_wrap(dataset~., nrow = 4, strip.position = "right") +
   geom_jitter(show.legend = F, size = 1.2, alpha = 0.7) +
-  geom_boxplot(alpha = 0.1, show.legend = F, size = 1.1, width = 0.8) +
+  geom_boxplot(alpha = 0.1, show.legend = F, size = 1.1, width = 0.8, outlier.alpha = 0) +
   theme_minimal(base_size = 16) +
   coord_cartesian(xlim = c(-0.3, 0.8)) +
   scale_x_continuous(breaks = seq(-0.5, 0.8, 0.1)) +
@@ -46,27 +46,27 @@ set.seed(42)
   scale_fill_manual(values = color_values) +
   geom_vline(xintercept = 0, color = 'black', linetype='dashed') +
   labs(y = element_blank(), x = bquote(R^2~"[10-fold cross validation]"),
-       title = 'Age Prediction From M/EEG Signals'))
+       title = 'Age Prediction From M/EEG Signals')
 
-my_ggsave('./figures/fig_performance_r2', fig_r2, dpi = 300, width = 8, height = 6)
+my_ggsave('./figures/fig_performance_r2', plot = fig_r2, dpi = 300, width = 8, height = 6)
 
 
 set.seed(42)
-(fig_mae <- ggplot(
+fig_mae <- ggplot(
   aes(x = MAE, y = benchmark, color = benchmark,
       fill = benchmark),
       data = results) +
   facet_wrap(dataset~., nrow = 4, strip.position = "right") +
   geom_jitter(show.legend = F, size = 1.2, alpha = 0.7) +
-  geom_boxplot(alpha = 0.1, show.legend = F, size = 1.1, width = 0.8) +
+  geom_boxplot(alpha = 0.1, show.legend = F, size = 1.1, width = 0.8, outlier.alpha = 0) +
   theme_minimal(base_size = 16) +
   scale_x_continuous(breaks = seq(0, 25, 1)) +
   scale_color_manual(values = color_values) +
   scale_fill_manual(values = color_values) +
   labs(y = element_blank(), x = "MAE [10-fold cross validation]",
-      title = 'Age Prediction From M/EEG Signals'))
+      title = 'Age Prediction From M/EEG Signals')
 
-my_ggsave('./figures/fig_performance_mae', fig_mae, dpi = 300,  width = 8, height = 6)
+my_ggsave('./figures/fig_performance_mae', plot = fig_mae, dpi = 300,  width = 8, height = 6)
 
 
 # Make table
